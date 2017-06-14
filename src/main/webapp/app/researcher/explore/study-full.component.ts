3197fdb5a97403a40995505df87bbeb2703ccfb0
Fabio Petrillo <fabio@petrillo.com>
2017-06-14 16:13:57 -0400

    Improving model and adding developer, systems and interviews

--- src/main/webapp/app/researcher/explore/study-full.component.ts
+++ src/main/webapp/app/researcher/explore/study-full.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
-import { EventManager  } from 'ng-jhipster';
+import { EventManager, ParseLinks, PaginationUtil, JhiLanguageService, AlertService } from 'ng-jhipster';
+
+import { ITEMS_PER_PAGE, Principal, ResponseWrapper } from '../../shared';
 
 import { Study } from '../../entities/study/study.model';
 import { StudyService } from '../../entities/study/study.service';
+
+import { Developer } from '../../entities/developer/developer.model';
+import { DeveloperService } from '../../entities/developer/developer.service';
+
+import { SoftwareSystem } from '../../entities/software-system/software-system.model';
+import { SoftwareSystemService } from '../../entities/software-system/software-system.service';
+
+import { Interview } from '../../entities/interview/interview.model';
+import { InterviewService } from '../../entities/interview/interview.service';
 
 @Component({
     selector: 'jhi-study-full',
@@ -13,12 +24,19 @@ import { StudyService } from '../../entities/study/study.service';
 export class StudyFullComponent implements OnInit, OnDestroy {
 
     study: Study;
+    developers: Developer[];
+    softwareSystems: SoftwareSystem[];
+    interviews: Interview[];
     private subscription: Subscription;
     private eventSubscriber: Subscription;
 
     constructor(
         private eventManager: EventManager,
         private studyService: StudyService,
+        private developerService: DeveloperService,
+        private softwareSystemService: SoftwareSystemService,
+        private interviewService: InterviewService,
+        private alertService: AlertService,
         private route: ActivatedRoute
     ) {
     }
@@ -34,7 +52,39 @@ export class StudyFullComponent implements OnInit, OnDestroy {
         this.studyService.find(id).subscribe((study) => {
             this.study = study;
         });
+
+        this.loadDevelopers(id);
+        this.loadSoftwareSystems(id);
+        this.loadInterviews(id);
     }
+
+    loadDevelopers(id) {
+      this.developerService.query().subscribe(
+            (res: ResponseWrapper) => {
+                this.developers = res.json;
+            },
+            (res: ResponseWrapper) => this.onError(res.json)
+        );
+    }
+
+    loadSoftwareSystems(id) {
+      this.softwareSystemService.query().subscribe(
+            (res: ResponseWrapper) => {
+                this.softwareSystems = res.json;
+            },
+            (res: ResponseWrapper) => this.onError(res.json)
+        );
+    }
+
+    loadInterviews(id) {
+      this.interviewService.query().subscribe(
+            (res: ResponseWrapper) => {
+                this.interviews = res.json;
+            },
+            (res: ResponseWrapper) => this.onError(res.json)
+        );
+    }
+
     previousState() {
         window.history.back();
     }
@@ -49,5 +99,9 @@ export class StudyFullComponent implements OnInit, OnDestroy {
             'studyListModification',
             (response) => this.load(this.study.id)
         );
+    }
+
+    private onError(error) {
+        this.alertService.error(error.message, null, null);
     }
 }
